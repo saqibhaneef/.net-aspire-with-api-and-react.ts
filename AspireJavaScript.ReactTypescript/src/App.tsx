13233964daaf9f3eb9feb5518e27dc922ea46ef1
saqibhaneef <muhammadsaqibhanif80@gmail.com>
2024-09-01 14:47:46 +0500

    Configure aspire api to consume in react typescript project

--- AspireJavaScript.ReactTypescript/src/App.tsx
+++ AspireJavaScript.ReactTypescript/src/App.tsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+/* eslint-disable no-debugger */
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+    const [forecasts, setForecasts] = useState([]);
+
+    const requestWeather = async () => {
+        const weather = await fetch("api/weatherforecast");
+        debugger
+        console.log(weather);
+
+        const weatherJson = await weather.json();
+        console.log(weatherJson);
+
+        setForecasts(weatherJson);
+    };
+
+    useEffect(() => {
+        requestWeather();
+    }, []);
 
   return (
     <>
@@ -27,7 +44,43 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+          </p>
+          <div className="App">
+              <header className="App-header">
+                  <h1>React Weather</h1>
+                  <table>
+                      <thead>
+                          <tr>
+                              <th>Date</th>
+                              <th>Temp. (C)</th>
+                              <th>Temp. (F)</th>
+                              <th>Summary</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          {(
+                              forecasts ?? [
+                                  {
+                                      date: "N/A",
+                                      temperatureC: "",
+                                      temperatureF: "",
+                                      summary: "No forecasts",
+                                  },
+                              ]
+                          ).map((w) => {
+                              return (
+                                  <tr key={w.date}>
+                                      <td>{w.date}</td>
+                                      <td>{w.temperatureC}</td>
+                                      <td>{w.temperatureF}</td>
+                                      <td>{w.summary}</td>
+                                  </tr>
+                              );
+                          })}
+                      </tbody>
+                  </table>
+              </header>
+          </div>
     </>
   )
 }
--- AspireJavaScript.ReactTypescript/vite.config.ts
+++ AspireJavaScript.ReactTypescript/vite.config.ts
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        port: parseInt(process.env.PORT) || 4001,
+        proxy: {
+            '/api': {
+                target: process.env.services__weatherapi__https__0 ||
+                process.env.services__weatherapi__http__0,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+                secure: false,
+            },
+        },
+    },
+    build: {
+        outDir: 'dist',
+    },
 })
